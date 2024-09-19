3afd33fc9dc553f2ff2517982763bea5631f2a0e
Leonardo Hernandes T. de S. Filho <leonardo.filho@ntsec.corp>
2024-09-19 15:32:30 -0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HomePage } from "./presentation/pages/Home/home_page.tsx";
+import Global_style from "./presentation/global/global_style.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Global_style />
+    <HomePage />
+  </StrictMode>,
+);
---
+++ src/presentation/pages/Home/home_page.tsx
@@ -0,0 +1,9 @@
+import { HomeContainer } from "./style";
+
+export const HomePage = () => {
+  return (
+    <HomeContainer>
+      <h1>init</h1>
+    </HomeContainer>
+  );
+};
